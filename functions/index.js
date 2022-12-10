bd3d87323e92c8cad13258a85b371e7d4ee2f017
Joshua Li <joshuazli2020@gmail.com>
2022-12-10 18:03:58 +1000

    init firebase app and connected twitter auth

---
+++ functions/index.js
@@ -0,0 +1,35 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const dbRef = admin.firestore().doc("tokens/demo");
+
+const TwitterApi = require("twitter-api-v2").default;
+const twitterClient = new TwitterApi({
+  clientId: "dXZKd2pEcjJzdnplOWxMdVYxbU46MTpjaQ",
+  clientSecret: "cIuLQks6-TkZhsHUHwisf0YVeXmZPH8IRm7EzbJQY6-KQYcEna",
+});
+
+const callbackURL =
+  "http://localhost:5000/twitter-tweeter-3cc43/us-central1/callback";
+
+exports.auth = functions.https.onRequest((request, response) => {
+  const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
+    callbackURL,
+    {
+      scope: ["tweet.read", "tweet.write", "users.read", "offline.access"],
+    }
+  );
+});
+
+exports.callback = functions.https.onRequest((request, response) => {});
+
+exports.tweet = functions.https.onRequest((request, response) => {});
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
